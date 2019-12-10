85ffb5335a00c573c9ce83da71751b3dda98e129
beaforesti <bea_fiore@live.it>
2019-12-10 19:54:14 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,104 @@
-function preload(){
+var graphics;
+var video;
+var capture;
+
+function preload() {
   // put preload code here
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  graphics = createGraphics(300, 300);
+  graphics.background('#660000');
+
+  video = createGraphics(640, 480);
+  video.background(0, 96, 128);
+  angleMode(DEGREES);
+
+  capture = createCapture(VIDEO);
+  capture.size(640, 480);
+  capture.hide();
 }
 
 function draw() {
-  // put drawing code here
+  // background("#003366");
+  background('#660000')
+  noStroke()
+
+  //lights
+  ambientLight(0)
+
+  //cheange point light
+  var locX = mouseX - height / 2;
+  var locY = mouseY - width / 2;
+
+  //turn on
+  if (keyIsDown(UP_ARROW)) {
+    ambientLight(200)
+    pointLight(255, locX, locY, locX, locY, 50);
+  }
+
+  //change camera on key pressed
+  if (keyIsDown(ENTER)) {
+    changeCamera()
+  }
+
+  //video from the webcam
+  video.image(capture, 0, 0, 640, 480)
+
+
+  //change colors of the sphere
+  for (var i = 0; i < graphics.height; i += 20) {
+    graphics.fill(map(mouseX, 0, width, 0, 255), map(mouseY, 0, height, 0, 255), random(255));
+    graphics.noStroke()
+    graphics.rect(0, i, graphics.height , 10);
+  }
+
+
+  //shapes
+  push()
+  rotateZ(frameCount * 0.5);
+  rotateX(frameCount * 0.5);
+  rotateY(frameCount * 0.5);
+  texture(video)
+  box(200, 200, 200, 2, 2)
+  pop()
+
+
+  push()
+  rotateZ(frameCount * 0.5);
+  rotateX(frameCount * 0.5);
+  translate(0, -300, -100)
+  texture(graphics)
+  sphere(80, 20, 20)
+  pop()
+
+  push()
+  rotateZ(frameCount * 0.5);
+  rotateY(frameCount * 0.5);
+  translate(200, -300, -100);
+  texture(graphics)
+  sphere(80, 20, 20)
+  pop()
+
+
+  push()
+  rotateX(frameCount * 0.5);
+  rotateY(frameCount * 0.5);
+  translate(100, -300, -100)
+  texture(graphics)
+  sphere(80, 20, 20)
+  pop()
+
+  //text
+  push()
+  fill(255)
+  text("UP ARROW to keep the light on, ENTER to change camera", 300, 300, 300, 10)
+  textSize(30)
+  pop()
+}
+
+
+function changeCamera() {
+  camera(200, 200, 200, 100, 0, 0, 1, 1, 0)
 }
